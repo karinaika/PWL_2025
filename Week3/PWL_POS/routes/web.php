ff64349c914f695d66806c2e8d959be4fcbb1e54
karinaika <karinaikaindasa@gmail.com>
2025-03-04 12:05:39 +0700

    Praktikum-6

--- Week3/PWL_POS/routes/web.php
+++ Week3/PWL_POS/routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\LevelController;
 use App\Http\Controllers\KategoriController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,5 +20,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/user', [UserController::class, 'index']);
 Route::get('/kategori', [KategoriController::class, 'index']);
 Route::get('/level', [LevelController::class, 'index']);
