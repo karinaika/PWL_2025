663683be422393ff7d6be0602dcd46d5187f89a2
karinaika <karinaikaindasa@gmail.com>
2025-03-08 16:18:34 +0700

    Praktikum-2.2

--- Week4/JS4/app/Http/Controllers/UserController.php
+++ Week4/JS4/app/Http/Controllers/UserController.php
@@ -10,29 +10,7 @@ class UserController extends Controller
 {
     public function index()
     {
-        $user = UserModel::findOr(20, ['username', 'nama'], function() {
-            abort(404);
-        });
+        $user = UserModel::where('username', 'manager9')->firstOrFail();
         return view('user', ['data' => $user]);
-        // $user = UserModel::findOr(1, ['username', 'nama'], function() {
-        //     abort(404);
-        // });
-        //$user = UserModel::firstWhere('level_id', 1);
-        //$user = UserModel::where('level_id', 1)->first();
-        //$user = UserModel::find(1); //untuk mencari data dengan user_id = 1.
-
-        // // tambah data user dengan Eloquent Model
-        // $data = [
-        //     'level_id' => 2, 
-        //     'username' => 'manager_tiga',  //'username' => 'manager_dua'
-        //     'nama' => 'Manager 3',
-        //     'password' => Hash::make('12345')
-        // ];
-        // UserModel::create($data);
-
-        // // akses model UserModel
-        // $user = UserModel::all(); // ambil semua data dari tabel m_user 
-
-        // return view('user', ['data' => $user]);
     }
 }
