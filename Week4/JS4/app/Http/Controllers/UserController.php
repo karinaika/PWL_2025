d234186611ae9fde5cb6947a51ddceebe27d795f
karinaika <karinaikaindasa@gmail.com>
2025-03-08 19:40:54 +0700

    Praktikum-2.7

--- Week4/JS4/app/Http/Controllers/UserController.php
+++ Week4/JS4/app/Http/Controllers/UserController.php
@@ -8,12 +8,17 @@ use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
+
+     // public function index()
+    // {
+    //     $user = UserModel::all();
+    //     return view('user', ['data' => $user]);
+    // }
     public function index()
     {
-        $user = UserModel::all();
+        $user = UserModel::with('level')->get();
         return view('user', ['data' => $user]);
     }
-
     public function tambah(){
         return view('user_tambah');
     }
