1915e0f8068e6ceaab4718bd6060c9231febf655
karinaika <karinaikaindasa@gmail.com>
2025-03-24 22:29:48 +0700

    Praktikum-2

--- Week5.2/JS5.2/routes/web.php
+++ Week5.2/JS5.2/routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\LevelController;
 use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -28,3 +29,5 @@ Route::get('/user/tambah', [UserController::class, 'tambah']);
 Route::get('/user', [UserController::class, 'index']);
 Route::get('/kategori', [KategoriController::class, 'index']);
 Route::get('/level', [LevelController::class, 'index']);
+
+Route::get('/', [WelcomeController::class, 'index']);
