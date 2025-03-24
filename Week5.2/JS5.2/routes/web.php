4fa2d77add6000f8c036d883790c4ee207636983
karinaika <karinaikaindasa@gmail.com>
2025-03-25 00:58:52 +0700

    Praktikum-3

--- Week5.2/JS5.2/routes/web.php
+++ Week5.2/JS5.2/routes/web.php
@@ -1,10 +1,8 @@
 <?php
 
-use App\Http\Controllers\LevelController;
-use App\Http\Controllers\KategoriController;
-use App\Http\Controllers\UserController;
 use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,17 +15,28 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [WelcomeController::class, 'index']);
+
+Route::group(['prefix' => 'user'], function () {
+    Route::get('/', [UserController::class, 'index']);           // menampilkan halaman awal user
+    Route::post('/list', [UserController::class, 'list']);       // menampilkan data user dalam bentuk json untuk datatables
+    Route::get('/create', [UserController::class, 'create']);    // menampilkan halaman form tambah user
+    Route::post('/', [UserController::class, 'store']);          // menyimpan data user baru
+    Route::get('/{id}', [UserController::class, 'show']);        // menampilkan detail user
+    Route::get('/{id}/edit', [UserController::class, 'edit']);   // menampilkan halaman form edit user
+    Route::put('/{id}', [UserController::class, 'update']);      // menyimpan perubahan data user
+    Route::delete('/{id}', [UserController::class, 'destroy']);  // menghapus data user
 });
 
-Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
-Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
-Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
-Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
-Route::get('/user/tambah', [UserController::class, 'tambah']);
-Route::get('/user', [UserController::class, 'index']);
-Route::get('/kategori', [KategoriController::class, 'index']);
-Route::get('/level', [LevelController::class, 'index']);
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/', [WelcomeController::class, 'index']);
+// Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
+// Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
+// Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
+// Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
+// Route::get('/user/tambah', [UserController::class, 'tambah']);
+// Route::get('/user', [UserController::class, 'index']);
+// Route::get('/kategori', [KategoriController::class, 'index']);
+// Route::get('/level', [LevelController::class, 'index']);
