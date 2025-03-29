b57a10fef5e2c904a10926d5ea67f936c5ee8a2b
karinaika <karinaikaindasa@gmail.com>
2025-03-29 22:12:35 +0700

    Praktikum 2 – Implementasi Authorizaton di Laravel dengan Middleware

--- Week7/JS7/routes/web.php
+++ Week7/JS7/routes/web.php
@@ -120,4 +120,15 @@ Route::middleware(['auth'])->group(function () {
          Route::delete('/{id}/delete_ajax', [SupplierController::class, 'delete_ajax']); // menghapus data Supplier ajax
         Route::delete('/{id}', [SupplierController::class, 'destroy']);
     });
+
+    // artinya semua route di dalam group ini harus punya role ADM (Administrator)
+    Route::middleware(['authorize:ADM'])->group(function(){
+        Route::get('/level', [LevelController::class, 'index']);
+        Route::post('/level/list', [LevelController::class, 'list']); // untuk list json datatables
+        Route::get('/level/create', [LevelController::class, 'create']);
+        Route::post('/level', [LevelController::class, 'store']);
+        Route::get('/level/{id}/edit', [LevelController::class, 'edit']); // untuk tampilkan form edit
+        Route::put('/level/{id}', [LevelController::class, 'update']); // untuk proses update data
+        Route::delete('/level/{id}', [LevelController::class, 'destroy']); // untuk proses hapus data
+    });
 });
