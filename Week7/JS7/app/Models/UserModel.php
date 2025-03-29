f5927c54cd6ac4debdf316c736f892786c032f45
karinaika <karinaikaindasa@gmail.com>
2025-03-29 23:25:25 +0700

    Praktikum 3 & Tugas 3 - Implementasi Multi-Level Authorization

--- Week7/JS7/app/Models/UserModel.php
+++ Week7/JS7/app/Models/UserModel.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
@@ -14,7 +13,9 @@ class UserModel extends Authenticatable
     protected $table = 'm_user';
     protected $primaryKey = 'user_id';
     protected $fillable = ['username', 'password', 'nama', 'level_id', 'created_at', 'updated_at'];
+
     protected $hidden = ['password']; // jangan di tampilkan saat select
+
     protected $casts = ['password' => 'hashed']; // casting password agar otomatis di hash
 
     /**
@@ -25,7 +26,7 @@ class UserModel extends Authenticatable
         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
     }
 
-    /**
+   /**
      * Mendapatkan nama role
      */
     public function getRoleName(): string
@@ -39,5 +40,13 @@ class UserModel extends Authenticatable
     public function hasRole($role): bool
     {
         return $this->level->level_kode == $role;
+    }
+
+    /**
+     * Mendapatkan kode role
+     */
+    public function getRole()
+    {
+        return $this->level->level_kode;
     }
 }
