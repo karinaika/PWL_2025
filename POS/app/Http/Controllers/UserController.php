b7292b1e359228f8fc7133a0f0b47f262c51a88a
karinaika <karinaikaindasa@gmail.com>
2025-04-10 07:19:36 +0700

    implementasi jobsheet 3

--- POS/app/Http/Controllers/UserController.php
+++ POS/app/Http/Controllers/UserController.php
@@ -2,12 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\UserModel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    public function showProfile($id, $name)
+    public function index()
     {
-        return view('user.profile', compact('id', 'name'));
+        // tambah data user dengan Eloquent Model
+        $data = [
+            'nama' => 'Pelanggan Pertama'
+        ];
+
+        // update data user
+        UserModel::where('username', 'customer-1')->update($data);
+
+        // coba akses model UserModel
+        $user = UserModel::all(); // ambil semua data dari tabel m_user 
+
+        return view('user', ['data' => $user]);
     }
 }
--- POS/routes/web.php
+++ POS/routes/web.php
@@ -1,10 +1,9 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\ProductController;
+use App\Http\Controllers\LevelController;
+use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\UserController;
-use App\Http\Controllers\SalesController;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,19 +16,10 @@ use App\Http\Controllers\SalesController;
 |
 */
 
-Route::get('/', [HomeController::class, 'index']);
-
-Route::prefix('category')->group(function () {
-    Route::get('/food-beverage', [ProductController::class, 'foodBeverage']);
-    Route::get('/beauty-health', [ProductController::class, 'beautyHealth']);
-    Route::get('/home-care', [ProductController::class, 'homeCare']);
-    Route::get('/baby-kid', [ProductController::class, 'babyKid']);
+Route::get('/', function () {
+    return view('welcome');
 });
 
-Route::get('/user/{id}/name/{name}', [UserController::class, 'showProfile']);
-
-Route::get('/sales', [SalesController::class, 'index']);
-
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/user', [UserController::class, 'index']);
+Route::get('/kategori', [KategoriController::class, 'index']);
+Route::get('/level', [LevelController::class, 'index']);
