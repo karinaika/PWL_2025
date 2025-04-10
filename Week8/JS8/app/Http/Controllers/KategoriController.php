bd9cec17a2cb47799e727ed71c282e910d5b45da
karinaika <karinaikaindasa@gmail.com>
2025-04-10 15:01:21 +0700

    Tugas 3 – Implementasi Export PDF pada Laravel : Data Kategori

--- Week8/JS8/app/Http/Controllers/KategoriController.php
+++ Week8/JS8/app/Http/Controllers/KategoriController.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\DB;
 use Yajra\DataTables\Facades\DataTables;
 use Illuminate\Support\Facades\Validator;
 use PhpOffice\PhpSpreadsheet\IOFactory;
+use Barryvdh\DomPDF\Facade\Pdf;
 
 class KategoriController extends Controller
 {
@@ -368,5 +369,20 @@ class KategoriController extends Controller
         $writer->save($filePath);
 
         return response()->download($filePath)->deleteFileAfterSend(true);
+    }
+
+    public function export_pdf()
+    {
+        $kategori = KategoriModel::select('kategori_kode', 'kategori_nama')
+            ->orderBy('kategori_id')
+            ->get();
+
+        // use Barryvdh\DomPDF\Facade\Pdf;
+        $pdf = Pdf::loadView('kategori.export_pdf', ['kategori' => $kategori]);
+        $pdf->setPaper('a4', 'portrait'); // set ukuran kertas dan orientasi
+        $pdf->setOption("isRemoteEnabled", true); // set true jika ada gambar dari url
+        $pdf->render();
+
+        return $pdf->stream('Data kategori ' . date('Y-m-d H:i:s') . '.pdf');
     }
 }
--- Week8/JS8/routes/web.php
+++ Week8/JS8/routes/web.php
@@ -79,6 +79,8 @@ Route::middleware(['auth'])->group(function () {
             Route::post('import_ajax', [LevelController::class, 'import_ajax']); // ajax import excel
             // Export Level with Excel
             Route::get('export_excel', [LevelController::class, 'export_excel']); //export excel
+            // Export Level with Pdf
+            Route::get('export_pdf', [LevelController::class, 'export_pdf']); //export pdf
         });
     });
 
@@ -107,6 +109,8 @@ Route::middleware(['auth'])->group(function () {
             Route::post('import_ajax', [KategoriController::class, 'import_ajax']); // ajax import excel
             // Export Barang with Excel
             Route::get('export_excel', [KategoriController::class, 'export_excel']); //export excel
+            // Export Kategori with Pdf
+            Route::get('export_pdf', [KategoriController::class, 'export_pdf']); //export pdf
         });
     });
 
