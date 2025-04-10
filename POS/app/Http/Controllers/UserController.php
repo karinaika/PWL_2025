96524a44615dc235af6f4a2da8c901088805f272
karinaika <karinaikaindasa@gmail.com>
2025-04-10 07:24:03 +0700

    implementasi jobsheet 4

--- POS/app/Http/Controllers/UserController.php
+++ POS/app/Http/Controllers/UserController.php
@@ -8,19 +8,53 @@ use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
+
+     // public function index()
+    // {
+    //     $user = UserModel::all();
+    //     return view('user', ['data' => $user]);
+    // }
     public function index()
     {
-        // tambah data user dengan Eloquent Model
-        $data = [
-            'nama' => 'Pelanggan Pertama'
-        ];
-
-        // update data user
-        UserModel::where('username', 'customer-1')->update($data);
-
-        // coba akses model UserModel
-        $user = UserModel::all(); // ambil semua data dari tabel m_user 
-
+        $user = UserModel::with('level')->get();
         return view('user', ['data' => $user]);
     }
+    public function tambah(){
+        return view('user_tambah');
+    }
+
+    public function tambah_simpan(Request $request){
+        UserModel::create([
+            'username' => $request->username,
+            'nama' => $request->nama,
+            'password' => Hash::make($request->password),
+            'level_id' => $request->level_id
+        ]);
+
+        return redirect('/user');
+    }
+
+    public function ubah($id){
+        $user=UserModel::find($id);
+        return view('user_ubah', ['data' => $user]);
+    }
+
+    public function ubah_simpan($id, Request $request){
+        $user=UserModel::find($id);
+        $user->username = $request->username;
+        $user->nama = $request->nama;
+        $user->password = hash::make('$request->password');
+        $user->level_id = $request->level_id;
+
+        $user->save();
+
+        return redirect('/user');
+    }
+
+    public function hapus($id){
+        $user=UserModel::find($id);
+        $user->delete();
+        return redirect('/user');
+    }
+
 }
