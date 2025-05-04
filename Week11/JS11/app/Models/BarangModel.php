9a1be6491778aa83a075df1c866290b90208a9e2
karinaika <karinaikaindasa@gmail.com>
2025-05-04 20:35:22 +0700

    Tugas 1 – Implementasi Eloquent Accessor (m_barang)

--- Week11/JS11/app/Models/BarangModel.php
+++ Week11/JS11/app/Models/BarangModel.php
@@ -5,9 +5,18 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
 class BarangModel extends Model
 {
+    public function getJWTIdentifier(){ 
+        return $this->getKey();
+    }
+    
+    public function getJWTCustomClaims(){ return [];
+    }
+
     use HasFactory;
 
     protected $table = "m_barang";
@@ -17,11 +26,24 @@ class BarangModel extends Model
         'barang_kode',
         'barang_nama',
         'harga_beli',
-        'harga_jual'
+        'harga_jual',
+        'image' // tambahan
     ];
 
     public function kategori(): BelongsTo
     {
         return $this->belongsTo(KategoriModel::class, 'kategori_id', 'kategori_id');
+    }
+
+    public function stok()
+    {
+        return $this->hasOne(StokModel::class, 'barang_id', 'barang_id');
+    }
+
+    protected function image(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($image) => $image ? url('/storage/barang/' . $image) : null
+        );
     }
 }
